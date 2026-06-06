7c7e8ece693fb4b6f801cd0d74e0a7965c4aa585
Jeremiah King <jeremiah@vols.utk.edu>
2026-06-06 19:15:48 -0400

    increased chunk size warning limit in vite.config.js
    added "gh-pages": "^5.0.0", to devDependencies

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/energy-dashboard/",
+  base: "./",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 700,
+  },
 });
