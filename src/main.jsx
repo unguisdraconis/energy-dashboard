779b0ebbdb755c495406cfb9560f19d10f3d7694
Jeremiah King <jeremiah@vols.utk.edu>
2026-06-12 20:56:17 -0400

    Annotated main.jsx with comments.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// Import necessary modules from React and ReactDOM libraries */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Import the main App component to be rendered in the browser */
+import App from "./App";
+
+/* Creates a root container for rendering the React application */
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
