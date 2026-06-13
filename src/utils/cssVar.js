6a5b54eb3ef65425fee9d163d6e130e067c72433
Jeremiah King <jeremiah@vols.utk.edu>
2026-06-12 21:17:43 -0400

    Annoted cssVar.js with general comment.

--- src/utils/cssVar.js
+++ src/utils/cssVar.js
@@ -1,3 +1,4 @@
+/* A utility property to retrieve the value of a CSS custom property (CSS variable) */
 export function cssVar(name) {
   return getComputedStyle(document.documentElement)
     .getPropertyValue(name)
